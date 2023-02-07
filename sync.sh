ebffc34cfce6d2bc210caa953dc2a9321306e0a4
Fernando Silva <fernandonsilva16@gmail.com>
2023-02-06 22:53:55 -0300

    feat(sync): sync (#76)

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@
 
 cp -r ../openpix-developers/docs/* ./docs/.
 cp -r ../openpix-developers/static/* ./static/.
-find docs -type f | xargs sed -i '' 's/OpenPix/Woovi/g'
-find docs -type f | xargs sed -i '' 's/openpix.com.br/woovi.com/g'
-find docs -type f | xargs sed -i '' 's/X-Woovi-Signature/X-OpenPix-Signature/g'
+find docs -type f \( -iname \*.md -o -iname \*.mdx \) | xargs sed -i '' 's/OpenPix/Woovi/g'
+find docs -type f \( -iname \*.md -o -iname \*.mdx \) | xargs sed -i '' 's/openpix.com.br/woovi.com/g'
+find docs -type f \( -iname \*.md -o -iname \*.mdx \) | xargs sed -i '' 's/openpix.com/woovi.com/g'
+find docs -type f \( -iname \*.md -o -iname \*.mdx \) | xargs sed -i '' 's/X-Woovi-Signature/X-OpenPix-Signature/g'
