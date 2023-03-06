294dd9cebc0f134332765adcc70cee8e5b426b99
Danilo <daniloassis.ti@gmail.com>
2023-03-06 11:16:48 -0300

    feat(qrc): api

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -x #echo on
 
-cp -r ../openpix-developers/docs/* ./docs/.
-cp -r ../openpix-developers/static/* ./static/.
+cp -r ../developers/docs/* ./docs/.
+cp -r ../developers/static/* ./static/.
 find docs -type f \( -iname \*.md -o -iname \*.mdx \) | xargs sed -i '' 's/OpenPix/Woovi/g'
 find docs -type f \( -iname \*.md -o -iname \*.mdx \) | xargs sed -i '' 's/openpix.com.br/woovi.com/g'
 find docs -type f \( -iname \*.md -o -iname \*.mdx \) | xargs sed -i '' 's/openpix.com/woovi.com/g'
